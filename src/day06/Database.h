71b893235f7e696e5468781e5c498ab3bd49fc40
Ziqiu Yang <yzq@email.com>
2025-08-08 23:35:59 +0800

    Update day06 folder.

---
+++ src/day06/Database.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Employee.h"
+
+namespace Records 
+{
+    const int kDefaultEmployeeNumber = 1000;
+
+    class Database
+    {
+        public:
+            Employee& addEmployee(const std::string& firstName,
+                                   const std::string& lastName);
+            Employee& getEmployee(int employeeNumber);
+            Employee& getEmployee(const std::string& firstName,  
+                                  const std::string& lastName);
+
+            void displayAll() const;
+            void displayCurrent() const;
+            void displayFormer() const;
+
+        
+        private:
+            std::vector<Employee> mEmployees;
+            int mNextEmployeeNumber = kDefaultEmployeeNumber;
+
+    };
+
+} // namespace Records
